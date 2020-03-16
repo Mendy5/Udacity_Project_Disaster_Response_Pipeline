ded1796ee978087ca9a7f0395a75d063e78c1428
Mendy Wu <wmd1103@hotmail.com>
2020-03-15 18:10:42 -0600

    add models directory 1. add machine learning pipeline

---
+++ models/train_classifier.py
@@ -0,0 +1,113 @@
+import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+import pandas as pd
+import numpy as np
+import re
+from sqlalchemy import create_engine, inspect
+import pickle
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics import confusion_matrix, classification_report
+
+
+def load_data(database_filepath):
+    engine = create_engine("sqlite:///{}".format(database_filepath))
+    df = pd.read_sql_table('Message', engine)
+    X = df['message'].values
+    Y = df.iloc[:, 5:].values
+    category_names = df.iloc[:, 5:].columns.values
+    return X, Y, category_names
+
+
+def tokenize(text):
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    #build pipeline
+    pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer=tokenize))
+    , ('tfidf', TfidfTransformer())
+    , ('clf', MultiOutputClassifier(RandomForestClassifier(), n_jobs=-1)) 
+    ])
+    
+    #add parameters
+    parameters =  {
+#         'vect__max_df': (0.5, 0.75),
+#         'vect__max_features': (None, 5000, 10000),
+        'tfidf__use_idf': (True, False),
+#         'clf__estimator__n_estimators' : [20, 50],
+#         'clf__estimator__min_samples_split' : [2, 5, 10],
+#         'clf__estimator__min_samples_leaf' : [1, 2, 4],
+        'clf__estimator__bootstrap' : (True, False)
+        }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    
+    return cv
+    
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    
+    for i in range(len(category_names)):
+        print(category_names[i], classification_report(Y_test[:,i], y_pred[:,i]))
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
